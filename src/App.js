4c120d14c76196b9a1f977066d3b47cbbaf0ac73
Tyrell McCurbin <tmccurbin@gmail.com>
2020-09-03 03:44:40 +0000

    Added tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonSegment, IonSegmentButton, IonPage } from '@ionic/react';
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonSegment, IonSegmentButton, IonPage, IonTab, IonTabs, IonTabBar, IonTabButton, IonLabel, IonRouterOutlet } from '@ionic/react';
+
+// Routing
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router-dom';
 
 // IMPORTANT: Import core CSS in order to get proper rendering
 // This is the first tier of CSS
@@ -22,24 +26,51 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
+// Page skeleton content (IonSegmentButtons and )
+const BrewPage = () => (
+  <IonPage>
+    <IonSegment value="cappuccino">
+      <IonSegmentButton value="espresso">Espresso</IonSegmentButton>
+      <IonSegmentButton value="macchiato">Macchiato</IonSegmentButton>
+      <IonSegmentButton value="cappuccino">Cappuccino</IonSegmentButton>
+    </IonSegment>
+    <IonButton expand="full">Brew</IonButton>
+  </IonPage>
+);
+
+const SettingsPage = () => (
+  <IonPage>Settings</IonPage>
+);
+
+
 function App() {
   return (
     <IonApp>
+      <IonReactRouter>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Brewster</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonPage>
-          <IonSegment value="cappuccino">
-            <IonSegmentButton value="espresso">Espresso</IonSegmentButton>
-            <IonSegmentButton value="macchiato">Macchiato</IonSegmentButton>
-            <IonSegmentButton value="cappuccino">Cappuccino</IonSegmentButton>
-          </IonSegment>
-          <IonButton expand="full">Brew</IonButton>
-        </IonPage>
+        <IonTabs>
+          {/*<!-- Look into IonRouterOutlet vs Switch -->*/}
+          <IonRouterOutlet>
+            <Route path="/" component={BrewPage} exact/>
+            <Route path="/settings" component={SettingsPage} exact/>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            {/*<!-- It seems you can name the tab properties whatever you want as long as the href is a proper route -->*/}
+            <IonTabButton tab="brew" href="/">
+              <IonLabel>Brew</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="settings" href="/settings">
+              <IonLabel>Settings</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
       </IonContent>
+      </IonReactRouter>
     </IonApp>
   );
 }
