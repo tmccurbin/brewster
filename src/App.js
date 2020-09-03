6b3da94cb0415e9a50d8a153446d6985d43d3536
Tyrell McCurbin <tmccurbin@gmail.com>
2020-09-03 01:48:30 +0000

    Install css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import './App.css';
 import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
 
 // IMPORTANT: Import core CSS in order to get proper rendering
+// This is the first tier of CSS
 // This can be places in index.js instead
 import '@ionic/react/css/core.css'
+
+// This is the second tier of CSS
+// This helpf to make the CSS consistent across devices
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+// This is the third tier of CSS
+// This gives the Ionic look and feel
+// This is optional
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 function App() {
   return (
