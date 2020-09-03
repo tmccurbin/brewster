58570028cfc31e4cc18fa4d839c4906f33cb9f66
Tyrell McCurbin <tmccurbin@gmail.com>
2020-09-03 01:34:08 +0000

    Initial implementation of Ionic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+
+// IMPORTANT: Import core CSS in order to get proper rendering
+// This can be places in index.js instead
+import '@ionic/react/css/core.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Brewster</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +28,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </IonContent>
+    </IonApp>
   );
 }
 
